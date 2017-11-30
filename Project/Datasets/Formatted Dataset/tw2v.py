b5a150086ab1a846d2b03391b6a915d4ea200150
Piradon Liengtiraphan <Piradon.Liengtiraphan1@gmail.com>
2017-11-30 09:57:24 -0500

    Modified tw2v to get max and average and upload all models needed

--- Project/Datasets/Formatted Dataset/tw2v.py
+++ Project/Datasets/Formatted Dataset/tw2v.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from sklearn.decomposition import TruncatedSVD
-'''from gensim.models import Word2Vec
-from gensim.models import word2vec'''
+from gensim.models import Word2Vec
+from gensim.models import word2vec
 from gensim.models import KeyedVectors
 import numpy as np
 import re
 
-# Create teh Model
+# Create the Model
 '''sentences = word2vec.Text8Corpus('text8')
 model = word2vec.Word2Vec(sentences, size = 200)
 model.save('text8.model')
@@ -20,6 +20,13 @@ print(model['sentence'].shape)
 
 x = pd.read_csv('formatted_dataset.csv', sep='^')
 
+# Get the length of the largest headliine and find the average
+headline_length_list = []
+for headline in range(len(x['Headline'])):
+    headline_length_list.append(len(x['Headline'][headline].split(" ")))
+
+max_length = max(headline_length_list)
+average = int(sum(headline_length_list) / len(headline_length_list))
 # for row, idx in df.iter_rows:
 # x['Headline']
 # x['Close Price']
@@ -41,18 +48,17 @@ wa = np.array(wa).T
 print(wa.shape)
 
 # zero padding to max length of sentence (words)
-mnwh = 100
+mnwh = max_length
 pwa = np.zeros((200, mnwh)) 
 pwa[:wa.shape[0],:wa.shape[1]] = wa
 
 print(pwa)
 print(pwa.shape)
 
-n = 15 # minimum or average size of 
+n = average # minimum or average size of 
 pca_n = TruncatedSVD(n_components=n, algorithm='arpack')
 pca_result_n = pca_n.fit_transform(pwa)
 print('Explained variation per principal component (PCA): {}'.format(np.sum(pca_n.explained_variance_ratio_)))
 
 print(pca_result_n)
 print(pca_result_n.shape)
-
