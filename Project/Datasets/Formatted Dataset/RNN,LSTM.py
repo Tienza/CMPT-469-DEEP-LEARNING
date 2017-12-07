1d54644d0bb67f88a301c9bede949ff6e8f8db10
Piradon (Tien) Liengtiraphan <Piradon.Liengtiraphan1@gmail.com>
2017-12-07 14:00:28 -0500

    Update RNN,LSTM.py

--- Project/Datasets/Formatted Dataset/RNN,LSTM.py
+++ Project/Datasets/Formatted Dataset/RNN,LSTM.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import time
 
-def next_batch(num, data, labels):
+def next_batch_train(num, data, labels):
     global trainImages
     global trainLabels
     idx = np.arange(0 , len(data))
@@ -21,8 +21,30 @@ def next_batch(num, data, labels):
     trainImages = trainImages[num:]
     trainLabels = trainLabels[num:]
 
-    print("trainImages Length: " + str(len(trainImages)))
-    print("trainLabels Length: " + str(len(trainLabels)))
+    #print("trainImages Length: " + str(len(trainImages)))
+    #print("trainLabels Length: " + str(len(trainLabels)))
+
+    return np.array(data_shuffle), np.array(labels_shuffle)
+
+def next_batch_test(num, data, labels):
+    global test_data
+    global test_label
+    idx = np.arange(0, len(data))
+    idx = idx[:num]
+    data_shuffle = [data[i] for i in idx]
+    labels_shuffle = [labels[i] for i in idx]
+
+    print(np.shape(data_shuffle))
+    print(np.shape(labels_shuffle))
+    print(np.shape(np.array(data_shuffle)))
+    print(np.shape(np.array(labels_shuffle)))
+
+    # Remove the num from the original lists
+    test_data = test_data[num:]
+    test_label = test_label[num:]
+
+    #print("test_data Length: " + str(len(test_data)))
+    #print("test_label Length: " + str(len(test_label)))
 
     return np.array(data_shuffle), np.array(labels_shuffle)
 
@@ -72,6 +94,9 @@ training_iters = 19975
 batch_size = 25
 display_step = 1
 
+test_data = testImages.reshape((-1, n_steps, n_input))
+test_label = testLabels
+
 #batch_x, batch_y = next_batch(batch_size, trainImages, trainLabels)
 #exit()
 
@@ -80,7 +105,7 @@ display_step = 1
 
 x = tf.placeholder(dtype="float", shape=[None, n_steps, n_input], name="x") # Current data input shape: (batch_size, n_steps, n_input) [100x28x28]
 y = tf.placeholder(dtype="float", shape=[None, n_classes], name="y")
-
+print(tf.shape(y))
 weights = {
     'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
 }
@@ -131,7 +156,7 @@ with tf.Session() as sess:
 
         # We will read a batch of 100 images [100 x 784] as batch_x
         # batch_y is a matrix of [100x10]
-        batch_x, batch_y = next_batch(batch_size, trainImages, trainLabels)
+        batch_x, batch_y = next_batch_train(batch_size, trainImages, trainLabels)
 
         # We consider each row of the image as one sequence
         # Reshape data to get 28 seq of 28 elements, so that, batxh_x is [100x28x28]
@@ -144,25 +169,31 @@ with tf.Session() as sess:
             # Calculate batch loss
             loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
             print("**************************************************************")
-            print("Iter " + str(step * batch_size) + ", Minibatch Loss = " + \
-                  "{:.6f}".format(loss))
+            print("Iter " + str(step * batch_size) + ", Minibatch Loss = " + "{:.6f}".format(loss))
             print("**************************************************************")
         step += 1
     print("Optimization Finished!")
 
     # Calculate accuracy for the testing data
-    # test_len = len(rand_index)
-    test_data = testImages.reshape((-1, n_steps, n_input))
-    test_label = testLabels
-    # Calculate batch loss
-    loss = sess.run(cost, feed_dict={x: test_data, y: test_label})
+    res = []
+    step = 0
+    while step < 4425:
+        batch_x, batch_y = next_batch_test(1, test_data, test_label)
+        tarY, predY = sess.run([y, pred], feed_dict={x: batch_x, y: batch_y})
+        # Convert to np arrays
+        tarY = np.asarray(tarY, dtype=np.float32)
+        predY = np.asarray(predY, dtype=np.float32)
+        res.append(np.fabs(tarY - predY))
+        step += 1
+
+    res = np.asarray(res, dtype=np.float32)
+    testing_average = np.mean(res)
+    print("**************************************************************")
+    print("Testing Mean: " + str(testing_average))
+    '''loss = sess.run(cost, feed_dict={x: test_data, y: test_label})
     print("**************************************************************")
     print("Iter " + str(step * batch_size) + ", Testing Batch Loss = " + \
           "{:.6f}".format(loss))
-    print("**************************************************************")
-
-    tarY, predY = sess.run(y, pred, feed_dict={x: test_data, y: test_label})
-    print(tarY)
-    print(predY)
+    print("**************************************************************")'''
 
 sess.close()
